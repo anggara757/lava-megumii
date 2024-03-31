9dc0f1b75ae2bf72e4d0dcacad59c681da171e50
anggara757 <63313530+anggara757@users.noreply.github.com>
2024-03-31 22:16:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d254cb3c-134b-4002-bfb0-9fc3c3fdca5b/)
 
 
 echo "$curl_output" >> Update.tsx
